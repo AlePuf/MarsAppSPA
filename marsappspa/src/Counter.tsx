b8a91dba55886809bd02655e2a83b9b76e932aaf
Alex Puflene <Alex.Puflene@softwire.com>
2022-07-01 16:23:41 +0300

    Added reset button for counter

--- marsappspa/src/Counter.tsx
+++ marsappspa/src/Counter.tsx
@@ -1,15 +1,27 @@
-import React, {useContext} from "react";
+import React, {useState, useContext} from "react";
 import Button from "@mui/material/Button";
 import {Context} from "./App";
 
 export function Counter() {
     const context = useContext(Context);
+    const [show, setShow] = useState(false);
     return (
         <div>
             <p>You clicked {context.count} times</p>
             <Button variant="outlined" color="success" onClick={() => context.setCount(context.count + 1)}>
                 Click me
             </Button>
+            <hr />
+            <Button variant="outlined" color="error" onClick={() => setShow(true)}>
+               Reset
+            </Button>
+            {show && <Button variant="outlined" color="error" onClick={() => {
+                window.alert("Counter reset");
+                setShow(false);
+                context.setCount(0);
+            }}>
+                Are you sure?
+            </Button>}
         </div>
     );
 }
